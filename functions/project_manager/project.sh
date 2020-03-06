0534620f46a103b99e4ae98568891a656a7b7438
Stuart Stone <stuart.stone@cloudreach.com>
2020-03-06 15:53:33 +0000

    delete test file

--- functions/project_manager/project.sh
+++
@@ -1,4 +0,0 @@
-curl -H "Content-Type: application/json" \
-	--request POST \
-	--data '{"name":"ban-and-impeached","billingAccount":"014289-16D89B-9466F5","parentId":"283639071922","parentType":"organization"}' \
-	http://localhost:8080
